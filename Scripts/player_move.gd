148ea7e5fd6acd52026ef0a5fc266eb548f13a95
eddbra73 <eddbra73@kingshigh.school.nz>
2025-06-27 09:51:44 +1200

    v1.8 Adding stuff
    I added the alien that gives you debuffs and kind of set up the easy level layout.

--- Scripts/player_move.gd
+++ Scripts/player_move.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
+@export var speed = 500
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func get_input():
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_direction * speed
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta):
+	get_input()
+	move_and_slide() 
