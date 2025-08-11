853050642cd303519c0b2b1fdc87e41e9fbf8406
eddbra73 <eddbra73@kingshigh.school.nz>
2025-08-11 13:23:38 +1200

    v1.96 more collision
    still haven't been able to make it respawn

--- Scripts/killzone.gd
+++ Scripts/killzone.gd
@@ -2,10 +2,10 @@ extends Area2D
 
 @onready var timer: Timer = $Timer
 
-func _on_body_entered(body):
+func _on_body_entered(body):    #when body/character enters the area/killzone
 	print("You died!") 
-	timer.start()
+	timer.start()     #starts time that stops it from instantly respawning, looks better
 
 
-func _on_timer_timeout() -> void:
-	get_tree().reload_current_scene() 
+func _on_timer_timeout() -> void:      #when timer ends do the following
+	get_tree().reload_current_scene()   #reloading main game scene so that you respawn at the start
--- Scripts/player_move.gd
+++ Scripts/player_move.gd
@@ -1,12 +1,28 @@
 extends CharacterBody2D
 
-@export var speed = 450
+@export var speed = 450      #setting speed of the main player
 
 func get_input():
-	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = input_direction * speed
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")    
+	#above is getting the direction to move the character from my inputs in the input map
+	velocity = input_direction * speed    #calculating velocity with the right direction and speed
 
 
 func _physics_process(delta):
-	get_input()
+	get_input()       
 	move_and_slide() 
+
+@export var respawn_position: Vector2
+
+func _ready():
+	respawn_position = global_position
+  
+
+func _on_DeadlyDetector_body_entered(body): 
+	if body == self:
+		respawn() 
+
+
+func respawn(): 
+	global_position = respawn_position
+	velocity = Vector2.ZERO
